ed57bdf2eb0ad1b03752b6ee9ccb8f8952e939e1
Sok Dina <sokdina999@gmail.com>
2016-09-30 07:13:29 -0700

    Add Test File

---
+++ src/main/java/www/muni/cz/Test.java
@@ -0,0 +1,13 @@
+package www.muni.cz;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test
+{
+    public int show_test( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
